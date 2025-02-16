18cebaf30edda6f382445aa477f79f11e4422749
Kenny Root <kenny@the-b.org>
2025-02-16 10:03:05 -0800

    Convert SlideInFadeOut to styled component

--- src/components/SlideInFadeOut/SlideInFadeOut.tsx
+++ src/components/SlideInFadeOut/SlideInFadeOut.tsx
@@ -1,17 +1,24 @@
 import { useState, useEffect } from "react";
-import "./SlideInFadeOut.css"; // Import the CSS file to handle animations
+import styled, { css } from "styled-components";
 
 interface SlideInFadeOutProps {
   children: React.ReactNode;
   onAnimationComplete: () => void;
+  horizontal?: boolean;
+  displayMs?: number;
+  restart?: boolean;
 }
 
 export const SlideInFadeOut = ({
   children,
   onAnimationComplete,
+  horizontal = false,
+  displayMs = 3000,
+  restart = false,
 }: SlideInFadeOutProps) => {
   const [isVisible, setIsVisible] = useState(false);
   const [isExiting, setIsExiting] = useState(false);
+  const [animationKey, setAnimationKey] = useState(0);
 
   useEffect(() => {
     // Start the visibility animation after a short delay
@@ -21,27 +28,60 @@ export const SlideInFadeOut = ({
 
     // Schedule the exit animation after 3 seconds
     const timeout = setTimeout(() => {
+      setIsVisible(false);
       setIsExiting(true);
-    }, 3000);
+    }, displayMs);
 
     return () => clearTimeout(timeout); // Cleanup the timeout on unmount
-  }, []);
+  }, [displayMs, animationKey]);
 
   useEffect(() => {
+    const animationCompleted = () => {
+      setIsVisible(false);
+      setIsExiting(false);
+
+      if (restart) {
+        setAnimationKey(animationKey + 1); // Restart the animation by incrementing the key
+      }
+      onAnimationComplete();
+    };
+
     if (isExiting) {
       // Call the callback function when the exit animation is complete
-      const timer = setTimeout(onAnimationComplete, 1000);
+      const timer = setTimeout(animationCompleted, 1000);
       return () => clearTimeout(timer);
     }
-  }, [isExiting, onAnimationComplete]);
+  }, [animationKey, isExiting, onAnimationComplete, restart]);
 
   return (
-    <div
-      className={`slide-in-fade-out ${isVisible ? "visible" : ""} ${
-        isExiting ? "exit" : ""
-      }`}
-    >
+    <Wrapper $isVisible={isVisible} $horizontal={horizontal}>
       {children}
-    </div>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.div<{ $horizontal?: boolean; $isVisible?: boolean }>`
+  transition: opacity 0.5s ease-in-out, transform 0.5s ease-in-out;
+  ${(props) =>
+    (props.$isVisible &&
+      css`
+        opacity: 0;
+        ${(props.$horizontal &&
+          css`
+            transform: translateX(100%);
+          `) ||
+        css`
+          transform: translateY(100%);
+        `}
+      `) ||
+    css`
+      opacity: 1;
+      ${(props.$horizontal &&
+        css`
+          transform: translateX(0%);
+        `) ||
+      css`
+        transform: translateY(0%);
+      `}
+    `}
+`;
